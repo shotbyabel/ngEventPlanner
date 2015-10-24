5f585823c20e8e28c1bb0bf4f4266c1ffbb817ff
kimo5532 <shotbyabel@gmail.com>
2015-10-24 16:43:05 -0700

    updated depedencies server js source code

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// require('dotenv').load();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -13,6 +15,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//server listening
+app.listen();
+console.log('Server is listening on port 3000!');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
